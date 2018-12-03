adafb7dae04b280a0a0a017379bb6d56952c8c84
Calebe <angelo.calebe@acad.pucrs.br>
2018-12-03 00:29:00 -0200

    N Rainhas

---
+++ 4_nqueens.py
@@ -0,0 +1,78 @@
+# coding: utf-8
+import sys, os, random, time
+
+class Board:
+    def __init__(self, size):
+        self.size = size
+        self.board = [[False for j in range(self.size)] for i in range(self.size)]
+        self.time = time.time()
+
+        os.system("clear")
+        if self.find_positions():
+            os.system("clear")
+            print(self)
+            pass
+        else:
+            os.system("clear")
+            print("Solução não encontrada. Último estado:")
+            print(self)
+
+    def __str__(self):
+        s = ""
+        for i in range(self.size):
+            for j in range(self.size):
+                s += "|"
+                s += "X" if self.board[i][j] else " "
+            s += "|" + os.linesep
+        return s
+
+    def find_positions(self, col = 0):
+        if col >= self.size:
+            return True
+
+        if col == 0:
+            i = random.randint(0, self.size - 1)
+            self.board[i][col] = True
+        
+        for i in range(self.size):
+            if self.is_safe(i, col):
+                if col != 0:
+                    self.board[i][col] = True
+                if time.time() - self.time > 0.03:
+                    self.time = time.time()
+                    os.system("clear")
+                    print(self)
+                if self.find_positions(col+1):
+                    return True
+                self.board[i][col] = False
+        return False
+
+    def is_safe(self, row, col):
+        for i in range(col):
+            if self.board[row][i]:
+                return False
+            
+        i = row
+        j = col
+        while i >= 0 and j >= 0:
+            if self.board[i][j]:
+                return False
+            i -= 1
+            j -= 1
+        
+        i = row
+        j = col
+        while i < self.size and j >= 0:
+            if self.board[i][j]:
+                return False
+            i += 1
+            j -= 1
+        
+        return True
+
+
+if len(sys.argv) < 2:
+    print("Uso: " + sys.argv[0] + " quantidade_de_rainhas")
+    quit()
+
+Board(int(sys.argv[1]))
